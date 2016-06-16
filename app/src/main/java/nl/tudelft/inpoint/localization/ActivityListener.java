6154572c854f1228443351165d0d4ca2a4fef9ab
Frits Kastelein <Frits@wlan-145-94-154-40.wlan.tudelft.nl>
2016-06-16 15:54:59 +0200

    updated probailities

--- app/src/main/java/nl/tudelft/inpoint/localization/ActivityListener.java
+++ app/src/main/java/nl/tudelft/inpoint/localization/ActivityListener.java
@@ -86,6 +86,101 @@ public class ActivityListener implements SensorEventListener {
         }
     }
 
+    private void moveProbabilities( int numberOfRoomsMoved ) {
+        int direction = Globals.getDirection();
+
+        for ( int x = 0; x < numberOfRoomsMoved; x++ ) {
+            if (direction == 0) { // EAST
+                for (int i = 1; i <= 3; i++) {
+                    Globals.POSTERIOR[i] = 0;
+                }
+
+                for (int i = 17; i >= 5; i--) {
+                    Globals.POSTERIOR[i] = Globals.POSTERIOR[i - 1];
+                }
+
+                for (int i = 18; i <= 21; i++) {
+                    Globals.POSTERIOR[i] = 0;
+                }
+            } else if (direction == 1) { // SOUTH
+                Globals.POSTERIOR[19] = Globals.POSTERIOR[18];
+
+                Globals.POSTERIOR[18] = Globals.POSTERIOR[5];
+                Globals.POSTERIOR[20] = Globals.POSTERIOR[12];
+                Globals.POSTERIOR[21] = Globals.POSTERIOR[16];
+
+                Globals.POSTERIOR[5] = Globals.POSTERIOR[1];
+                Globals.POSTERIOR[12] = Globals.POSTERIOR[2];
+                Globals.POSTERIOR[16] = Globals.POSTERIOR[3];
+
+                Globals.POSTERIOR[1] = 0;
+                Globals.POSTERIOR[2] = 0;
+                Globals.POSTERIOR[3] = 0;
+                Globals.POSTERIOR[4] = 0;
+                Globals.POSTERIOR[6] = 0;
+                Globals.POSTERIOR[7] = 0;
+                Globals.POSTERIOR[8] = 0;
+                Globals.POSTERIOR[9] = 0;
+                Globals.POSTERIOR[10] = 0;
+                Globals.POSTERIOR[11] = 0;
+                Globals.POSTERIOR[13] = 0;
+                Globals.POSTERIOR[14] = 0;
+                Globals.POSTERIOR[15] = 0;
+            } else if (direction == 2) { // WEST
+                for (int i = 1; i <= 3; i++) {
+                    Globals.POSTERIOR[i] = 0;
+                }
+
+                for (int i = 4; i <= 16; i--) {
+                    Globals.POSTERIOR[i + 1] = Globals.POSTERIOR[i];
+                }
+
+                for (int i = 18; i <= 21; i++) {
+                    Globals.POSTERIOR[i] = 0;
+                }
+            } else if (direction == 3) { // North
+                Globals.POSTERIOR[1] = Globals.POSTERIOR[5];
+                Globals.POSTERIOR[2] = Globals.POSTERIOR[12];
+                Globals.POSTERIOR[3] = Globals.POSTERIOR[16];
+
+                Globals.POSTERIOR[5] = Globals.POSTERIOR[18];
+                Globals.POSTERIOR[12] = Globals.POSTERIOR[20];
+                Globals.POSTERIOR[16] = Globals.POSTERIOR[21];
+
+                Globals.POSTERIOR[18] = Globals.POSTERIOR[19];
+
+                Globals.POSTERIOR[4] = 0;
+                Globals.POSTERIOR[6] = 0;
+                Globals.POSTERIOR[7] = 0;
+                Globals.POSTERIOR[8] = 0;
+                Globals.POSTERIOR[9] = 0;
+                Globals.POSTERIOR[10] = 0;
+                Globals.POSTERIOR[11] = 0;
+                Globals.POSTERIOR[13] = 0;
+                Globals.POSTERIOR[14] = 0;
+                Globals.POSTERIOR[15] = 0;
+                Globals.POSTERIOR[19] = 0;
+                Globals.POSTERIOR[20] = 0;
+                Globals.POSTERIOR[21] = 0;
+            }
+
+            float sum = 0f;
+            for (int i = 1; i <= 21; i++) {
+                sum += Globals.POSTERIOR[i];
+            }
+
+            if (sum == 0) {
+                for (int i = 1; i <= 21; i++) {
+                    Globals.POSTERIOR[i] = 1f / 21f;
+                }
+            } else {
+                for (int i = 1; i <= 21; i++) {
+                    Globals.POSTERIOR[i] /= sum;
+                }
+            }
+        }
+    }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
